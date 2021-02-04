97933ba27e7366aa7ffd6410e968b3396b10e03a
muminae100 <smuminaetx100@gmail.com>
2021-02-04 20:23:03 +0300

    modified learning.py

--- learning.py
+++ learning.py
@@ -1,25 +1,24 @@
 from flask import Flask,render_template,jsonify,request
 app = Flask(__name__)
 
-books = [
+books =  {'id': 0,
+           'title': 'A Fire Upon the Deep',
+           'author': 'Vernor Vinge',
+           'first_sentence': 'The coldsleep itself was dreamless.',
+           'year_published': '1992'
+           }
+#     {'id': 1,
+#      'title': 'The Ones Who Walk Away From Omelas',
+#      'author': 'Ursula K. Le Guin',
+#      'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+#      'published': '1973'},
+#     {'id': 2,
+#      'title': 'Dhalgren',
+#      'author': 'Samuel R. Delany',
+#      'first_sentence': 'to wound the autumnal city.',
+#      'published': '1975'}
 
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-
-]
+# ]
 @app.route('/')
 def home_page():
         return render_template("test.html")
